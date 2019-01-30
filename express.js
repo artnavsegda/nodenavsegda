bdd44d9f44b6d38a9dcfa6bba4ec1f50cf1ca7c6
Art Navsegda <artnavsegda@gmail.com>
2019-01-31 02:04:00 +0300

    fixings

--- express.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const PORT = process.env.PORT || 5000
-
-express()
-	.use(express.static("static"))
-	.listen(PORT, () => console.log(`Listening on ${ PORT }`)) 
-
