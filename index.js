148b62f90217b175a2a87c59e3a3a52694b30632
Art Navsegda <artnavsegda@gmail.com>
2019-02-01 05:15:06 +0300

    address

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var mongo = require('mongodb').MongoClient('mongodb://localhost:27017');
-//const mongo = require('mongodb').MongoClient('mongodb://artnavsegda:dep7k36c@ds129051.mlab.com:29051/artnavsegda');
+//var mongo = require('mongodb').MongoClient('mongodb://localhost:27017');
+const mongo = require('mongodb').MongoClient('mongodb://artnavsegda:dep7k36c@ds129051.mlab.com:29051/artnavsegda');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
