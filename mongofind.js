b1ba19ae5e1fdd148dbfe40b9fc24236010564b7
Art Navsegda <artnavsegda@gmail.com>
2019-02-01 03:57:06 +0300

    mlab hosting

--- mongofind.js
+++ mongofind.js
@@ -1,8 +1,9 @@
-var MongoClient = require('mongodb').MongoClient('mongodb://localhost:27017');
+//var MongoClient = require('mongodb').MongoClient('mongodb://localhost:27017');
+var MongoClient = require('mongodb').MongoClient('mongodb://artnavsegda:dep7k36c@ds129051.mlab.com:29051/artnavsegda');
 
 MongoClient.connect((err) => {
 	if (err) throw err;
-	const db = MongoClient.db('test');
+	const db = MongoClient.db('artnavsegda');
 	db.collection('blog').find({}).toArray((err,res) => {
 		if (err) throw err;
 		console.log(res);
--- mongoinsert.js
+++ mongoinsert.js
@@ -1,9 +1,10 @@
-var MongoClient = require('mongodb').MongoClient('mongodb://localhost:27017');
+//var MongoClient = require('mongodb').MongoClient('mongodb://localhost:27017');
+var MongoClient = require('mongodb').MongoClient('mongodb://artnavsegda:dep7k36c@ds129051.mlab.com:29051/artnavsegda');
 
 MongoClient.connect((err) => {
 	if (err) throw err;
 	console.log("Connected successfully to server");
-	const db = MongoClient.db('test');
+	const db = MongoClient.db('artnavsegda');
 	db.collection('blog').insertOne({ name: "hello"}, (err,res) => {
 		if (err) throw err;
 		console.log("1 document inserted");
