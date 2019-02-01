2ddb8d9fc9ebd2cf493f44e6a6b8f3432d680af7
Art Navsegda <artnavsegda@gmail.com>
2019-02-01 03:01:07 +0300

    mongo

--- mongoinsert.js
+++ mongoinsert.js
@@ -1,12 +1,12 @@
 var MongoClient = require('mongodb').MongoClient('mongodb://localhost:27017');
 
-MongoClient.connect((err,db) => {
+MongoClient.connect((err) => {
 	if (err) throw err;
 	console.log("Connected successfully to server");
-	db.db('test').collection('blog').insert({ name: "hello"}, (err,res) => {
+	const db = MongoClient.db('test');
+	db.collection('blog').insertOne({ name: "hello"}, (err,res) => {
 		if (err) throw err;
 		console.log("1 document inserted");
-		db.close();
+		MongoClient.close();
 	});
-	MongoClient.close();
 });
