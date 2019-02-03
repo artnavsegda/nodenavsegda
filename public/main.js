60dc4f66e12e28ad7ecadb51ed70c1344d4cc5d8
Art Navsegda <artnavsegda@gmail.com>
2019-02-03 06:00:50 +0300

    fix

--- public/main.js
+++ public/main.js
@@ -35,8 +35,10 @@ function appendclear()
 	    if (this.readyState == 4 && this.status == 200) {
 	       JSON.parse(xhttp.responseText).forEach( (element) => {
 					 var node = document.createElement("OPTION");
+					 var textnode = document.createTextNode(element.name);
 					 node.value = element.name;
-					 document.getElementById("browsers").appendChild(node);
+					 node.appendChild(textnode);
+					 document.getElementsByName("browser")[0].appendChild(node);
 					 //innerHTML += element.name;
 	       })
 	    }
