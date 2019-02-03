d066827967ba9a8101948c8cfb0bef0d7489ac21
Art Navsegda <artnavsegda@gmail.com>
2019-02-03 06:51:41 +0300

    define

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require('express')
 const path = require('path')
+const querystring = require('querystring');
+const https = require('https');
 const PORT = process.env.PORT || 5000
 
 var MongoClient = require('mongodb').MongoClient;
+
+function httpsget(address, callback) {
+	let data = '';
+	https.get(address, (res) => {
+		res.on('data', (chunk) => {
+			data += chunk;
+		});
+		res.on('end', () => {
+			typeof callback === 'function' && callback (data);
+	  });
+	}).on('error', (e) => {
+		console.error(e);
+	});
+}
+
+function vkAPIcall(method, parameters, vk_callback)
+{
+	httpsget("https://api.vk.com/method/" + method + "?" + querystring.stringify(parameters), vk_callback);
+}
+
+function sendVKmessage(usertosend, messagetosend, my_access_token)
+{
+	vkAPIcall("messages.send", {access_token: my_access_token, v: 5.92, user_id: usertosend, random_id: Math.random() * 123456789, message: messagetosend});
+}
 
 function mongouse(callback)
 {
@@ -39,5 +65,9 @@ express()
 				res.send("success");
 			});
 		});
+	})
+	.get('/sendvkmessage', (req, res) => {
+		sendVKmessage(185014513, "hello", req.query.browser);
+		res.send("success");
 	})
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
