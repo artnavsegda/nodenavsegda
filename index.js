a6f5d624a9fc7e147eabc204908f6110ac4ea9f1
Art Navsegda <artnavsegda@gmail.com>
2019-02-03 05:42:58 +0300

    refactor

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-//var mongo = require('mongodb').MongoClient('mongodb://localhost:27017');
-const mongo = require('mongodb').MongoClient('mongodb://artnavsegda:dep7k36c@ds129051.mlab.com:29051/artnavsegda');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var MongoClient = require('mongodb').MongoClient;
 
+function mongouse(callback)
+{
+	MongoClient.connect('mongodb://artnavsegda:dep7k36c@ds129051.mlab.com:29051/artnavsegda', function (err, client) {
+//	MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+		if (err) throw err;
+		var db = client.db('artnavsegda');
+		callback(db);
+	})
+}
 
 express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.get('/hello', (req, res) => {
-		mongo.connect((err) => {
-			if (err) throw err;
-			const db = mongo.db('artnavsegda');
+		mongouse((db) => {
 			db.collection('blog').find({}).toArray((err,query) => {
 				if (err) throw err;
 				res.send(query);
@@ -19,9 +25,7 @@ express()
 		});
 	})
 	.get('/clear', (req, res) => {
-		mongo.connect((err) => {
-			if (err) throw err;
-			const db = mongo.db('artnavsegda');
+		mongouse((db) => {
 			db.collection('blog').deleteOne({ name: req.query.browser}, (err,resource) => {
 				if (err) throw err;
 				res.send("success");
@@ -29,9 +33,7 @@ express()
 		});
 	})
 	.get('/append', (req, res) => {
-		mongo.connect((err) => {
-			if (err) throw err;
-			const db = mongo.db('artnavsegda');
+		mongouse((db) => {
 			db.collection('blog').insertOne({ name: req.query.browser}, (err,resource) => {
 				if (err) throw err;
 				res.send("success");
