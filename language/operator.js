4f425b06acf7756cc6e71e493437720f9c13a035
Art Navsegda <artnavsegda@gmail.com>
2019-07-13 22:07:29 +0300

    Create operator.js

---
+++ language/operator.js
@@ -0,0 +1,18 @@
+let i;
+i = 42 + 20;
+console.log(i);
+i++;
+console.log(i);
+i--;
+console.log(i);
+i = 42 - 20;
+console.log(i);
+i = 13 * 1.5;
+console.log(i);
+i = 42 / 2;
+console.log(i);
+i = 5 % 3;
+console.log(i);
+i = "hello" + "world";
+console.log(i);
+
