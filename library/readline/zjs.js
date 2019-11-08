961e221cb6667e762eb6e1d4bdb99db753b9fdd4
Art Navsegda <artnavsegda@gmail.com>
2019-11-09 02:00:16 +0300

    modi

--- library/readline/zjs.js
+++ library/readline/zjs.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface(
 });
 
 function completer(line) {
-  const completions = '.help .error .exit .quit .q'.split(' ');
+  const completions = 'help error exit quit q'.split(' ');
   const hits = completions.filter((c) => c.startsWith(line));
   // Show all completions if none found
   return [hits.length ? hits : completions, line];
