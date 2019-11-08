31ff99ca043dfe57ca850fecacd5f212bb9cfe11
Art Navsegda <artnavsegda@gmail.com>
2019-11-09 02:05:45 +0300

    mo

--- library/child/spawn.js
+++ library/child/spawn.js
@@ -5,10 +5,6 @@ ls.stdout.on('data', (data) => {
   console.log(`stdout: ${data}`);
 });
 
-ls.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
-});
-
 ls.on('close', (code) => {
   console.log(`child process exited with code ${code}`);
 });
