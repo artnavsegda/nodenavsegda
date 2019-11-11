f46556dac724d71f4d8897fd8153ac746807393b
Art Navsegda <artnavsegda@gmail.com>
2019-11-11 16:05:11 +0300

    interp

--- zjs/zjs.js
+++ zjs/zjs.js
@@ -16,23 +16,6 @@ rl.on('close', shutdown);
 config.readconfig();
 rl.prompt();
 
-function interpret(line)
-{
-  switch (line.trim()) {
-    case 'hello':
-      console.log('world!');
-      break;
-    case 'config':
-      config.readconfig();
-      console.log(config.configpath);
-      break;
-    default:
-      console.log(`Say what? I might have heard '${line.trim()}'`);
-      break;
-  }
-  rl.prompt();
-}
-
 function shutdown()
 {
   console.log('Have a great day!');
