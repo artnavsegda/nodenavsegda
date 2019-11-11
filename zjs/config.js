048148986d6cda1fbb51fe2f552ea530e10e9c7e
Art Navsegda <artnavsegda@gmail.com>
2019-11-11 16:13:30 +0300

    zocm

--- zjs/config.js
+++ zjs/config.js
@@ -1,11 +1,16 @@
-exports.readconfig = function() {
+const fs = require('fs');
+
+exports.configpath = configpath;
+exports.schemapath = schemapath;
+exports.readconfig = readconfig;
+
+function readconfig() {
   console.log("ha ha");
+//  console.log(fs.readFileSync('test2.txt', 'utf8'));
 }
 
 var configpath = "/config/path";
 
 var schemapath;
 
-exports.configpath = configpath;
-exports.schemapath = schemapath;
 
