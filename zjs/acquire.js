b47911243efe19608c2f63fe3ab4cd87f9a19760
Art Navsegda <artnavsegda@gmail.com>
2019-11-12 10:05:10 +0300

    aaa

--- zjs/acquire.js
+++ zjs/acquire.js
@@ -1,12 +1,16 @@
 exports.acquire = acquire;
 exports.acquireall = acquireall;
 
-function acquire()
+function acquire(proto)
 {
-
+  console.log(proto);
 }
 
-function acquireall(root)
+function acquireall(directory)
 {
-
+  for (var key in directory)
+  {
+    console.log(key);
+    acquire(directory[key]);
+  }
 }
