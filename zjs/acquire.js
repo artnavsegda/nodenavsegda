a7b7d275cb6e03db1c208149d8e741b3e250b8a4
Art Navsegda <artnavsegda@gmail.com>
2019-11-12 11:33:14 +0300

    aaaa

--- zjs/acquire.js
+++ zjs/acquire.js
@@ -1,13 +1,16 @@
-const { exec, spawn } = require('child_process');
+const { exec, spawn, execFileSync } = require('child_process');
 
 exports.acquire = acquire;
 exports.acquireall = acquireall;
 
 function acquire(proto)
 {
-  //console.log(proto);
-  console.log(proto.schema.acquire.shell)
-  console.log(proto.schema.acquire.args)
+  //console.log(proto.schema.acquire.shell)
+  //console.log(proto.schema.acquire.args)
+  //console.log(String(execFileSync(proto.schema.acquire.shell, proto.schema.acquire.args)));
+  //console.log(JSON.parse(execFileSync(proto.schema.acquire.shell, proto.schema.acquire.args)));
+  proto.data = JSON.parse(execFileSync(proto.schema.acquire.shell, proto.schema.acquire.args));
+  console.log(proto);
 }
 
 function acquireall(directory)
