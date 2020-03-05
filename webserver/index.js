79fcee666a61715419c55f08c1e4c570d8515dba
Art Navsegda <artnavsegda@gmail.com>
2020-03-05 16:00:49 +0300

    Update index.js

--- webserver/index.js
+++ webserver/index.js
@@ -4,4 +4,11 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/status', (req, res) => {res.send('status')})
+
+app.get('/command', (req, res) => {
+  console.log("executing");
+  res.send('complete');
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
