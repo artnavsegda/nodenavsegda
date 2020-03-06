7762ea4df4e1c65f4c242ae7ff02442768a713d5
Art Navsegda <artnavsegda@gmail.com>
2020-03-06 14:55:43 +0300

    Update index.js

--- webclient/index.js
+++ webclient/index.js
@@ -10,3 +10,15 @@ request({
   console.log('body:', body); // Print the HTML for the Google homepage.
   console.log('currentState:', body.currentState);
 });
+
+request({
+  url: 'http://localhost:3000/command',
+  body: {'targetState': on},
+  method: 'POST',
+  headers: {'Content-type': 'application/json'}
+}, function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log('currentState:', body.currentState);
+});
