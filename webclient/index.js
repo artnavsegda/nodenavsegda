370153ddf66798ada6b7a0cb710b4616b51fc6f4
Art Navsegda <artnavsegda@gmail.com>
2020-03-06 14:37:42 +0300

    nda

--- webclient/index.js
+++ webclient/index.js
@@ -1,7 +1,11 @@
 const request = require('request');
 
-request('http://localhost:3000/', function (error, response, body) {
+request({
+  url: 'http://localhost:3000/status',
+  method: 'GET'
+}, function (error, response, body) {
   console.error('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  //console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log('currentState:', body.currentState);
 });
--- webserver/index.js
+++ webserver/index.js
@@ -6,7 +6,7 @@ var state = false;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/status', (req, res) => {res.send('status')})
+app.get('/status', (req, res) => {res.send({currentState: 100})})
 
 app.get('/command', (req, res) => {
   console.log("executing");
