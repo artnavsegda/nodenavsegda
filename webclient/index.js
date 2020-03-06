a695e9cf12b5f9c8bf292e15292b74a70831b241
Art Navsegda <artnavsegda@gmail.com>
2020-03-06 15:13:25 +0300

    Update index.js

--- webclient/index.js
+++ webclient/index.js
@@ -20,5 +20,4 @@ request({
   console.error('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
-  console.log('currentState:', body.currentState);
 });
