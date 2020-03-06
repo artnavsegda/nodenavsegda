4d437a7ddfd05f461e084f6c48981854da6b6d0a
Art Navsegda <artnavsegda@gmail.com>
2020-03-06 14:42:13 +0300

    Update index.js

--- webclient/index.js
+++ webclient/index.js
@@ -2,10 +2,11 @@ const request = require('request');
 
 request({
   url: 'http://localhost:3000/status',
-  method: 'GET'
+  method: 'GET',
+  json: true
 }, function (error, response, body) {
   console.error('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log('body:', body); // Print the HTML for the Google homepage.
   console.log('currentState:', body.currentState);
 });
