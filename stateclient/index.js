0e422c7b636d951892cfcab94d28c406223b8652
Art Navsegda <artnavsegda@gmail.com>
2020-03-12 02:16:46 +0300

    stateclient

---
+++ stateclient/index.js
@@ -0,0 +1,44 @@
+const request = require('request');
+
+setTimeout(function() {
+  request({
+    url: 'http://localhost:3000/status',
+    method: 'GET',
+    json: true
+  }, function (error, response, body) {
+    console.log("!GET!");
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    console.log('currentState:', body.currentState);
+  });
+}, 500);
+
+setTimeout(function() {
+  request({
+    url: 'http://localhost:3000/command',
+    body: {currentState: true},
+    method: 'POST',
+    json: true,
+    headers: {'Content-type': 'application/json'}
+  }, function (error, response, body) {
+    console.log("!POST!");
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
+}, 1000);
+
+setTimeout(function() {
+  request({
+    url: 'http://localhost:3000/status',
+    method: 'GET',
+    json: true
+  }, function (error, response, body) {
+    console.log("!GET!");
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    console.log('currentState:', body.currentState);
+  });
+}, 1500);
