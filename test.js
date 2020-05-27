e3d48381360300ad0df39288fc361089754bbca9
artnavsegda@gmail.com <artnavsegda@gmail.com>
2020-05-27 11:51:16 +0300

    a

--- test.js
+++ test.js
@@ -12,6 +12,46 @@ var test_data = {
   one: "raz"
 }
 
+function testschema(schema, data)
+{
+  var valid = true;
+  var element;
+
+  for (condition of schema.modificator)
+  {
+    for (element of condition.if)
+    {
+      console.log(element);
+      console.log(data);
+
+      if (element == data) {
+        console.log("match");
+        valid = true;
+      } else {
+        console.log("not match");
+        valid = false;
+        break;
+      }
+    }
+  }
+  return valid;
+}
+
+if (testschema(test_schema, test_data)){
+  console.log("valid");
+} else {
+  console.log("invalid");
+}
+
+
+
+
+////////////////
+
+
+
+
+
 var subject_element = {
   one: "raz",
   two: "dva"
@@ -21,22 +61,6 @@ var test_element = {
   one: "raz",
   two: "dva",
   two: "three"
-}
-
-function testsubject2(subject, test)
-{
-  var valid = true;
-  var element;
-  for (element of Object.getOwnPropertyNames(test))
-  {
-    if (test[element] == subject[element]){
-      valid = true;
-    } else {
-      valid = false;
-      break;
-    }
-  }
-  return valid;
 }
 
 function testsubject(subject, test)
@@ -53,10 +77,4 @@ function testsubject(subject, test)
     }
   }
   return valid;
-}
-
-if (testsubject(subject_element, test_element)){
-  console.log("valid");
-} else {
-  console.log("invalid");
 }
