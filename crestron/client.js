ab840631af0dc87a505de9658fdfb0eed2e518a2
Art Navsegda <artnavsegda@gmail.com>
2020-06-11 15:06:00 +0300

    Update client.js

--- crestron/client.js
+++ crestron/client.js
@@ -2,20 +2,18 @@ const net = require('net');
 
 const client = net.createConnection({ port: 6666, host: "192.168.88.41"}, () => {
   console.log('connected to server!');
-  client.write('D0001V00001');
+  //client.write('D0001V00001');
 });
 
 client.on('data', (data) => {
-  console.log("first listener" + data.toString());
+  console.log("first listener " + data.toString());
 
-
-  //client.end();
 });
 
-client.on('data', (data) => {
-  console.log("second listener" + data.toString());
-  //client.end();
-});
+// client.on('data', (data) => {
+//   console.log("second listener " + data.toString());
+//   //client.end();
+// });
 
 client.on('end', () => {
   console.log('disconnected from server');
