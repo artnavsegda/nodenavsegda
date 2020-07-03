3f1b4e26c9b3375618b88fee629820b401ff2efe
Art Navsegda <artnavsegda@gmail.com>
2020-07-03 16:20:28 +0300

    Update index.js

--- fetch/index.js
+++ fetch/index.js
@@ -1,4 +1,5 @@
-fetch('https://github.com/')
+const fetch = require('node-fetch');
+
+fetch('http://192.168.88.41:7001/R0001')
     .then(res => res.text())
     .then(body => console.log(body));
-    
