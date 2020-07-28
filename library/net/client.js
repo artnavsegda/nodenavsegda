478e7a5bd9c1170984e42356abdcf580af8a4e4e
Art Navsegda <artnavsegda@gmail.com>
2020-07-28 14:01:49 +0300

    Update client.js

--- library/net/client.js
+++ library/net/client.js
@@ -1,13 +1,13 @@
 const net = require('net');
 
-const client = net.createConnection(9999, "192.168.88.41", () => {
+const client = net.createConnection(6666, "192.168.88.41", () => {
   console.log('connected to server!');
-  client.write('world!\r\n');
+  //client.write('world!\r\n');
 });
 
 client.on('data', (data) => {
   console.log(data.toString());
-  client.end();
+  //client.end();
 });
 
 client.on('end', () => {
