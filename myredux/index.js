ea2b80b94e341897724d72a00956d52536506795
Art Navsegda <artnavsegda@gmail.com>
2020-09-03 11:33:55 +0300

    Update index.js

--- myredux/index.js
+++ myredux/index.js
@@ -1,2 +1,43 @@
-var createStore = require('redux').createStore;
+const createStore = require('redux').createStore;
 
+const initialState = {
+    1:{
+      myLight: false
+    },
+    2:{
+      myLight: false
+    },
+    3:{
+      myLight: false
+    },
+    4:{
+      myLight: false
+    }
+  };
+  
+  const reducer = (state = initialState, action) => {
+    if (typeof state === 'undefined') {
+      return 0
+    }
+  
+    let newstate = {...state};
+  
+    switch (action.type) {
+      case 'TOGGLE':
+        newstate[action.id].myLight = !newstate[action.id].myLight;
+        return newstate;
+      break;
+      case 'ON':
+        newstate[action.id].myLight = true;
+        return newstate;
+      break;
+      case 'OFF':
+        newstate[action.id].myLight = false;
+        return newstate;
+      break;
+      default:
+        return state;
+    }
+  }
+
+const store = createStore(reducer);
