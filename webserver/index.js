814266b6c118a95f5c0706a4524b83d3720cd88d
Art Navsegda <artnavsegda@gmail.com>
2020-09-08 12:26:55 +0300

    Update index.js

--- webserver/index.js
+++ webserver/index.js
@@ -3,13 +3,13 @@ const app = express()
 app.use(express.json())
 const port = 3000
 
-var dictionary = ["hi","hello","cat", "dog"];
+var dictionary = ["hi","hello","cat", "dog", "car", "bird", "danger", "love", "computer"];
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/q', (req, res) => {
   console.log("query " + req.query.ask);
-  res.send(dictionary);
+  res.send(dictionary.filter((element) => element.startsWith(req.query.ask)));
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
