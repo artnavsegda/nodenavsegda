29337b2b04f0daf5a9eb2a4b9dcf3a82d9cadd97
Art Navsegda <artnavsegda@gmail.com>
2020-09-29 11:39:02 +0300

    apollo

--- mygraphql/server.js
+++ mygraphql/server.js
@@ -1,26 +1,23 @@
-var express = require('express');
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+const typeDefs = gql`
   type Query {
     hello: String
   }
-`);
+`;
 
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello world!';
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
   },
 };
 
-var app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log('Now browse to http://localhost:4000' + server.graphqlPath)
+);
