a6b747e911c5238d545186ab02b3b8e0b170f2e7
Art Navsegda <artnavsegda@gmail.com>
2020-10-02 14:44:14 +0300

    Update test3.js

--- test3.js
+++ test3.js
@@ -4,4 +4,13 @@ const myobject = {
     lamp3: {on: true, brightness: 100}
 }
 
-console.log(JSON.stringify(myobject));
+//console.log(JSON.stringify(myobject));
+
+//for (const [key, value] of Object.entries(myobject)) {
+    //console.log(`${key}: ${value}`);
+//}
+
+console.log(JSON.stringify(Object.entries(myobject).map((what) => {
+    what[1].ID = what[0];
+    return what[1];
+})));
