20b8cc371adfe7bf280388584a702be9d0409773
Art Navsegda <artnavsegda@gmail.com>
2020-10-22 16:27:32 +0300

    Update cip.js

--- crestron/cip.js
+++ crestron/cip.js
@@ -1,1 +1,16 @@
+const net = require('net');
+
 //cip protocol scaffold
+
+const client = net.createConnection({ port: 41794, host: "192.168.88.41"}, () => {
+    console.log('connected to server');
+});
+
+client.on('data', (data) => {
+    console.log(data.toString('hex'));
+    console.log("type: 0x" + data[0].toString(16));
+});
+
+client.on('end', () => {
+    console.log('disconnected from server');
+});
