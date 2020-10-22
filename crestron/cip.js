d1854aa38ffc59c6c67763e87e5019a3c6933c7f
Art Navsegda <artnavsegda@gmail.com>
2020-10-22 16:40:15 +0300

    Update cip.js

--- crestron/cip.js
+++ crestron/cip.js
@@ -8,7 +8,28 @@ const client = net.createConnection({ port: 41794, host: "192.168.88.41"}, () =>
 
 client.on('data', (data) => {
     console.log(data.toString('hex'));
-    console.log("type: 0x" + data[0].toString(16));
+
+    let payloadType = data[0];
+    console.log("type: 0x" + payloadType.toString(16));
+
+    switch (payloadType)
+    {
+        case 0x0f:
+            console.log("Client registration request");
+            client.write("\x01\x00\x0b\x00\x00\x00\x00\x00" + "\x03" + "\x40\xff\xff\xf1\x01");
+        break;
+        case 0x02:
+            console.log("registration ok");
+            client.write("\x05\x00\x05\x00\x00\x02\x03\x00");
+        break;
+        case 0x05:
+            console.log("data");
+        break;
+        case 0x0D:
+            console.log("heartbeat");
+        break;
+    }
+
 });
 
 client.on('end', () => {
