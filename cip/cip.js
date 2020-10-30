1bfd39c5653e930478ee9f9f742027f2ef1f6cfd
Art Navsegda <artnavsegda@gmail.com>
2020-10-30 15:15:55 +0300

    Update cip.js

--- cip/cip.js
+++ cip/cip.js
@@ -68,4 +68,7 @@ client.on('end', () => {
 
 const app = express()
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/test', (req, res) => {
+    res.send('Hello World!')
+});
 app.listen(3000, () => console.log(`Example app listening at http://localhost:3000`))
