5615d59e5d65165d02bf3279273c5fa5c1285b82
Art Navsegda <artnavsegda@gmail.com>
2020-10-30 21:34:49 +0300

    Create lexicalcalculator.js

---
+++ lexicalcalculator.js
@@ -0,0 +1,20 @@
+const zero = () => {}
+const one = () => {}
+const two = () => {}
+const three = () => {}
+const four = () => {}
+const five = () => {}
+const six = () => {}
+const seven = () => {}
+const eight = () => {}
+const nine = () => {}
+
+const plus = () => {}
+const minus = () => {}
+const multiply = () => {}
+const dividedBy = () => {}
+
+console.log(seven(multiply(five()))) // must return 35
+console.log(four(plus(nine()))) // must return 13
+console.log(eight(minus(three()))) // must return 5
+console.log(six(dividedBy(two()))) // must return 3
