8c3376de130c599cd998261f967d3e7e4a6a04b8
Art Navsegda <artnavsegda@gmail.com>
2020-11-03 14:45:01 +0300

    cip

--- cip/index.js
+++ cip/index.js
@@ -1,1 +1,22 @@
-import cipclient from './cip.js'
+const express = require('express');
+const cipclient = require('./cip.js');
+
+const cip = cipclient.connect({host: "192.168.88.41", ipid: "\x03"}, () => {
+    console.log('CIP connected');
+})
+
+cip.subscribe((data) => {
+    console.log("type:" + data.type + " join:" + data.join + " value:" + data.value);
+});
+
+const app = express()
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/test', (req, res) => {
+    //cip.aset(33,50);
+    //cip.dset(210, 1);
+    //cip.dset(210, 0);
+    //cip.pulse(202);
+    console.log("result " + cip.dget(1));
+    res.send('Hello World!');
+});
+app.listen(3000, () => console.log(`Example app listening at http://localhost:3000`))
