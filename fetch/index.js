7722a1f137c0b045ad98be2b7d9c2d24571d80ed
Art Navsegda <artnavsegda@gmail.com>
2020-11-26 18:10:25 +0300

    Update index.js

--- fetch/index.js
+++ fetch/index.js
@@ -1,4 +1,12 @@
 const fetch = require('node-fetch');
+
+const api = 'https://app.tseh85.com/DemoService/api';
+
+const auth = api + '/AuthenticateVending';
+
+console.log(auth);
+
+/*
 const { Headers } = require('node-fetch');
 
 let authHeader = new Headers({'Authorization':'Bearer eyJhcGlfa2V5IjoiNzVkMzc3N2M3NWFhM2QwOTkxOWEyZTI4ZjhiM2M1YTkifQ=='})
@@ -6,3 +14,4 @@ let authHeader = new Headers({'Authorization':'Bearer eyJhcGlfa2V5IjoiNzVkMzc3N2
 fetch('https://api.simplecast.com/podcasts/', {headers: authHeader})
     .then(res => res.text())
     .then(body => console.log(body));
+ */
