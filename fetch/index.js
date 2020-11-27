6ded667b36f18c50c8b09fc3c64a013cf4f79e66
Art Navsegda <artnavsegda@gmail.com>
2020-11-27 11:52:57 +0300

    Update index.js

--- fetch/index.js
+++ fetch/index.js
@@ -6,24 +6,21 @@ const auth = api + '/AuthenticateVending';
 
 const machines = api + '/vending/machines'
 
-fetch(machines)
+/* fetch(machines)
   .then(response => response.text())
-  .then(text => console.log(text))
+  .then(text => console.log(text)) */
 
 
-/* const payload = {
+const payload = {
     "Login": "sample string 1",
-    "Password": "sample string 2",
-    "DeviceGUID": "sample string 3",
-    "PushNotificationToken": "sample string 4"
 }
 
 fetch(auth, {
     method: 'POST',
     headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'text/json'
     },
     body: JSON.stringify(payload)
 })
   .then(response => response.text())
-  .then(text => console.log(text)) */
+  .then(text => console.log(text+"!!!!"))
