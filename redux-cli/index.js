54b9d7bc36389456eb4885b402ab4f2f373cf792
artnavsegda <artnavsegda@gmail.com>
2020-11-29 18:53:14 +0300

    Update index.js

--- redux-cli/index.js
+++ redux-cli/index.js
@@ -1,4 +1,21 @@
 const readline = require('readline');
+const redux = require('redux');
+
+function counterReducer(state = { value: 0 }, action) {
+    switch (action.type) {
+      case 'counter/incremented':
+        return { value: state.value + 1 }
+      case 'counter/decremented':
+        return { value: state.value - 1 }
+      default:
+        return state
+    }
+}
+
+let store = redux.createStore(counterReducer)
+
+store.subscribe(() => console.log(store.getState()))
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -9,9 +26,12 @@ rl.prompt();
 
 rl.on('line', (line) => {
   switch (line.trim()) {
-    case 'hello':
-      console.log('world!');
-      break;
+    case 'inc':
+        store.dispatch({ type: 'counter/incremented' })
+    break;
+    case 'dec':
+        store.dispatch({ type: 'counter/decremented' })
+    break;
     default:
       console.log(`Say what? I might have heard '${line.trim()}'`);
       break;
