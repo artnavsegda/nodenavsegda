c8072c740a42a76dbefea0cfe14afaa4ada85968
Art Navsegda <artnavsegda@gmail.com>
2021-02-12 03:57:10 +0300

    a

--- test2.js
+++ test2.js
@@ -1,38 +1,445 @@
-// Unit: Workshop
+let list = [{
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Garage]Ceiling[L0-1]",
+	"displayName": "Гараж L0.1",
+	"setOn": 1,
+	"setOff": 101,
+	"getOn": 1
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Corridor]Ceiling[L0-2]",
+	"displayName": "Коридор L0.2",
+	"setOn": 2,
+	"setOff": 102,
+	"getOn": 2
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Boiler]Ceiling[L0-3]",
+	"displayName": "Бойлерная L0.3",
+	"setOn": 3,
+	"setOff": 103,
+	"getOn": 3
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Technical_room]Ceiling[L0-4]",
+	"displayName": "Техническая комната L0.4",
+	"setOn": 4,
+	"setOff": 104,
+	"getOn": 4
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Stairs]Bra[Ll1]",
+	"displayName": "Бра Лл1",
+	"setOn": 5,
+	"setOff": 105,
+	"getOn": 5
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Stairs]Bra[Ll2]",
+	"displayName": "Бра Лл2",
+	"setOn": 6,
+	"setOff": 106,
+	"getOn": 6
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Hallway]Ceiling[L1-1]",
+	"displayName": "Прихожая L1.1",
+	"setOn": 7,
+	"setOff": 107,
+	"getOn": 7
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Hall]Ceiling[L2-1]",
+	"displayName": "Потолок L2.1",
+	"setOn": 8,
+	"setOff": 108,
+	"getOn": 8
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Hall]Bra[L2-1/1]",
+	"displayName": "Бра L2.1/1",
+	"setOn": 9,
+	"setOff": 109,
+	"getOn": 9
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Hall]LED[L2-2]",
+	"displayName": "LED L2.2",
+	"setOn": 10,
+	"setOff": 110,
+	"getOn": 10
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Hall]Ceiling[L2-3]",
+	"displayName": "Потолок L2.3",
+	"setOn": 11,
+	"setOff": 111,
+	"getOn": 11
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Hall]LED[L2-4/1]",
+	"displayName": "LED L2.4/1",
+	"setOn": 12,
+	"setOff": 112,
+	"getOn": 12
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Hall]LED[L2-4/2]",
+	"displayName": "LED L2.4/2",
+	"setOn": 13,
+	"setOff": 113,
+	"getOn": 13
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Hall]Lamp[L2-5]",
+	"displayName": "Встроенный L2.5",
+	"setOn": 14,
+	"setOff": 114,
+	"getOn": 14
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Hall]LED[L2-6]",
+	"displayName": "LED L2.6",
+	"setOn": 15,
+	"setOff": 115,
+	"getOn": 15
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Living_room]Luster[L3-1]",
+	"displayName": "Люстра L3.1",
+	"setOn": 16,
+	"setOff": 116,
+	"getOn": 16
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Living_room]LED[L3-2]",
+	"displayName": "LED L3.2",
+	"setOn": 17,
+	"setOff": 117,
+	"getOn": 17
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Living_room]LED[L3-3]",
+	"displayName": "LED L3.3",
+	"setOn": 18,
+	"setOff": 118,
+	"getOn": 18
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Living_room]LED[L3-4]",
+	"displayName": "LED L3.4",
+	"setOn": 19,
+	"setOff": 119,
+	"getOn": 19
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Kitchen]Luster[L4-1]",
+	"displayName": "Люстра L4.1",
+	"setOn": 20,
+	"setOff": 120,
+	"getOn": 20
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Kitchen]Built-in[L4-2]",
+	"displayName": "Встроенный L4.2",
+	"setOn": 21,
+	"setOff": 121,
+	"getOn": 21
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Kitchen]LED[L4-2/1]",
+	"displayName": "LED L4.2/1",
+	"setOn": 22,
+	"setOff": 122,
+	"getOn": 22
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Kitchen]Built-in[L4-3]",
+	"displayName": "Встроенный при входе L4.3",
+	"setOn": 23,
+	"setOff": 123,
+	"getOn": 23
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bathroom]Bra[L5-1]",
+	"displayName": "Бра L5.1",
+	"setOn": 24,
+	"setOff": 124,
+	"getOn": 24
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bathroom]Luster[L5-2]",
+	"displayName": "Люстра L5.2",
+	"setOn": 25,
+	"setOff": 125,
+	"getOn": 25
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bedroom]Luster[L6-1]",
+	"displayName": "Люстра L6.1",
+	"setOn": 26,
+	"setOff": 126,
+	"getOn": 26
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bedroom]LED[L6-3]",
+	"displayName": "LED L6.3",
+	"setOn": 27,
+	"setOff": 127,
+	"getOn": 27
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bedroom]Bra[L6-5]",
+	"displayName": "Бра L6.5",
+	"setOn": 28,
+	"setOff": 128,
+	"getOn": 28
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bedroom]LED[L6-6]",
+	"displayName": "LED L6.6",
+	"setOn": 29,
+	"setOff": 129,
+	"getOn": 29
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bedroom]Bra[L6-7]",
+	"displayName": "Бра L6.7",
+	"setOn": 30,
+	"setOff": 130,
+	"getOn": 30
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bedroom]Bra[L6-8]",
+	"displayName": "Бра L6.8",
+	"setOn": 31,
+	"setOff": 131,
+	"getOn": 31
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bedroom]Outlet[L6-9]",
+	"displayName": "Розетка L6.9",
+	"setOn": 32,
+	"setOff": 132,
+	"getOn": 32
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bedroom]Outlet[L6-10]",
+	"displayName": "Розетка L6.10",
+	"setOn": 33,
+	"setOff": 133,
+	"getOn": 33
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Kids_room]Ceiling[L7-1]",
+	"displayName": "Потолок L7.1",
+	"setOn": 34,
+	"setOff": 134,
+	"getOn": 34
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Kids_room]Ceiling[L7-2]",
+	"displayName": "Потолок L7.2",
+	"setOn": 35,
+	"setOff": 135,
+	"getOn": 35
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Kids_room]Bra[L7-3]",
+	"displayName": "Бра L7.3",
+	"setOn": 36,
+	"setOff": 136,
+	"getOn": 36
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Kids_room]Bra[L7-4]",
+	"displayName": "Бра L7.4",
+	"setOn": 37,
+	"setOff": 137,
+	"getOn": 37
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Kids_room]Ceiling[L7-5]",
+	"displayName": "Потолок L7.5",
+	"setOn": 38,
+	"setOff": 138,
+	"getOn": 38
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Kids_room]Ceiling[L7-6]",
+	"displayName": "Потолок L7.6",
+	"setOn": 39,
+	"setOff": 139,
+	"getOn": 39
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Kids_room]Bra[L7-7]",
+	"displayName": "Бра L7.7",
+	"setOn": 40,
+	"setOff": 140,
+	"getOn": 40
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Kids_room]Bra[L7-8]",
+	"displayName": "Бра L7.8",
+	"setOn": 41,
+	"setOff": 141,
+	"getOn": 41
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bedroom_bathroom]Ceiling[L8-1]",
+	"displayName": "Потолок L8.1",
+	"setOn": 42,
+	"setOff": 142,
+	"getOn": 42
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bedroom_bathroom]Mirror[L8-2]",
+	"displayName": "Зеркало L8.2",
+	"setOn": 43,
+	"setOff": 143,
+	"getOn": 43
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Kids_room_bathroom]Ceiling[L9-1]",
+	"displayName": "Потолок L9.1",
+	"setOn": 44,
+	"setOff": 144,
+	"getOn": 44
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Kids_room_bathroom]Mirror[L9-2]",
+	"displayName": "Зеркало L9.2",
+	"setOn": 45,
+	"setOff": 145,
+	"getOn": 45
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Hall]Bra[L10-1]",
+	"displayName": "Template",
+	"setOn": 46,
+	"setOff": 146,
+	"getOn": 46
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Hall]Ceiling[L10-2]",
+	"displayName": "Потолок L10.2",
+	"setOn": 47,
+	"setOff": 147,
+	"getOn": 47
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Cabinet]Ceiling[L11-1]",
+	"displayName": "Потолок L11.1",
+	"setOn": 48,
+	"setOff": 148,
+	"getOn": 48
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Cabinet]Ceiling[L11-2]",
+	"displayName": "Потолок L11.2",
+	"setOn": 49,
+	"setOff": 149,
+	"getOn": 49
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Cabinet]Bra[L11-3]",
+	"displayName": "Бра L11.3",
+	"setOn": 50,
+	"setOff": 150,
+	"getOn": 50
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Cabinet]LED[L11-4]",
+	"displayName": "LED L11.4",
+	"setOn": 51,
+	"setOff": 151,
+	"getOn": 51
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Cabinet]Luster[Ll3]",
+	"displayName": "Люстра Ll3",
+	"setOn": 52,
+	"setOff": 152,
+	"getOn": 52
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Hall]Bra[L12-1]",
+	"displayName": "Бра L12.1",
+	"setOn": 53,
+	"setOff": 153,
+	"getOn": 53
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bathroom]Lamp[L13-1]",
+	"displayName": "Лампа L13.1",
+	"setOn": 54,
+	"setOff": 154,
+	"getOn": 54
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bathroom]Bra_mirror[L13-2]",
+	"displayName": "Зеркало L13.2",
+	"setOn": 55,
+	"setOff": 155,
+	"getOn": 55
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bathroom][L13-3]",
+	"displayName": "L13.3",
+	"setOn": 56,
+	"setOff": 156,
+	"getOn": 56
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Bathroom]LED[L13-4]",
+	"displayName": "LED L13.4",
+	"setOn": 57,
+	"setOff": 157,
+	"getOn": 57
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][1st_level]Lamp[L14-1]",
+	"displayName": "Лампа L14.1",
+	"setOn": 58,
+	"setOff": 158,
+	"getOn": 58
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][1st_level]Lamp[L14-2]",
+	"displayName": "Лампа L14.2",
+	"setOn": 59,
+	"setOff": 159,
+	"getOn": 59
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][1st_level]Bra[L14-3]",
+	"displayName": "Бра L14.3",
+	"setOn": 60,
+	"setOff": 160,
+	"getOn": 60
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][1st_level]Bra[L14-4]",
+	"displayName": "Бра L14.4",
+	"setOn": 61,
+	"setOff": 161,
+	"getOn": 61
+}, {
+	"type": "Lightbulb",
+	"uniqueId": "[Light][Balcony]Lamp[L15-1]",
+	"displayName": "Балкон L15.1",
+	"setOn": 62,
+	"setOff": 162,
+	"getOn": 62
+}].map((element) => {
+    return {
+        name: element.displayName,
+        type: "dimmerlights",
+        is_On: element.uniqueId + "[is_On]",
+        set_On: element.uniqueId + "[On]",
+        set_Off: element.uniqueId + "[Off]"
+    }
+})
 
-function workshopLightMenu(minilist)
-{
-   this.Show_page = function(){
-      IR.ShowPopup("workshopLightMenu", 2001);
-   };
-   this.Close_page = function(){};
-   var roomPage = IR.CreateItem(IR.ITEM_POPUP, "workshopLightMenu", 0, 0, 640, 1136);
-   roomPage.GetState(0).Color = backgroundColor;
-   var roomFancyHeader = new LightsPageHeader(roomPage, "Свет", 0, 0, 640, 73, minilist, this);
-   
-   FancyHeader(roomPage, "МАСТЕРСКАЯ", 0, 73, 640, 73);
-   
-   var lightTileContainer = new TileContainer(roomPage, "Мастерская", 0, 146, 640, 1063);
-    [
-        {n: "Лампа L14.1", s: "[Light][1st_level]Lamp[L14-1]"},
-        {n: "Лампа L14.2", s: "[Light][1st_level]Lamp[L14-2]"},
-        {n: "Бра L14.3", s: "[Light][1st_level]Bra[L14-3]"},
-        {n: "Бра L14.4", s: "[Light][1st_level]Bra[L14-4]"},
-    ].forEach(function(element) {
-        lightTileContainer.add({
-            name: element.n,
-            stateID: element.s,
-            icon: 'a', 
-            onRelease: function() {
-                Pulse(element.s + "[Toggle]")
-            },
-            onLongPress: function(){
-                tileModal({
-                    title: element.n,
-                    levelIn: element.s + "[Level]", 
-                    levelOut: element.s + "[Level]",
-                });
-            }
-         });
-    });
-}
+console.log(JSON.stringify(list))
