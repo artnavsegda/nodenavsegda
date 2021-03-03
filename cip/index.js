f9dd5b7b64798758d39c534cc4cc6ae55edf7311
Art Navsegda <artnavsegda@gmail.com>
2021-03-03 04:26:25 +0300

    Update index.js

--- cip/index.js
+++ cip/index.js
@@ -1,7 +1,7 @@
 import cipclient from 'crestron-cip';
 import express from 'express';
 
-const cip = cipclient.connect({host: "192.168.88.41", ipid: "\x03"}, () => {
+const cip = cipclient.connect({host: "192.168.10.10", ipid: "\x03"}, () => {
     console.log('CIP connected');
 })
 
@@ -14,10 +14,10 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/test', (req, res) => {
     //cip.aset(33,50);
     //cip.dset(210, 1);
-    //cip.dset(210, 0);
+    cip.dset(501, 1);
     //cip.pulse(202);
-    console.log("result " + cip.dget(1));
-    console.log("result " + cip.aget(1));
+    //console.log("result " + cip.dget(1));
+    //console.log("result " + cip.aget(1));
     res.send('Hello World!');
 });
 app.listen(3000, () => console.log(`Example app listening at http://localhost:3000`))
