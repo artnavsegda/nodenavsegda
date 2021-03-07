414c31d27b5656a45d798d2396365c4c272d7d6a
Art Navsegda <artnavsegda@gmail.com>
2021-03-07 21:01:40 +0300

    Update coolmaster.js

--- fetch/coolmaster.js
+++ fetch/coolmaster.js
@@ -49,7 +49,6 @@ async function get_mode_old() {
 }
 
 async function set_mode(mode) {
-    
     let response = await fetch('http://192.168.10.12:10103/v1.0/device/283B96003138/raw?command=heat&L1_001');
     //let response = await fetch('http://192.168.10.12:10103/v1.0/device/283B96003138/raw?command=cool&L1_001');
     let data = await response.json();
