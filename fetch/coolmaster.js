68ea754c5385f0dd93db2bab74139fa990744652
Art Navsegda <artnavsegda@gmail.com>
2021-03-07 04:00:31 +0300

    Update coolmaster.js

--- fetch/coolmaster.js
+++ fetch/coolmaster.js
@@ -12,9 +12,23 @@ async function is_on() {
     return Number(data.data[0]);
 }
 
+async function set_on() {
+    let response = await fetch('http://192.168.10.12:10103/v1.0/device/283B96003138/raw?command=on&L1_001');
+    let data = await response.json();
+    return data.rc;
+}
+
+async function set_off() {
+    let response = await fetch('http://192.168.10.12:10103/v1.0/device/283B96003138/raw?command=off&L1_001');
+    let data = await response.json();
+    return data.rc;
+}
+
 async function coolmaster_status() {
     //console.log(JSON.stringify(await full_status()))
     console.log(JSON.stringify(await is_on()))
+    //console.log(JSON.stringify(await set_on()))
+    //console.log(JSON.stringify(await set_off()))
 }
 
 coolmaster_status();
