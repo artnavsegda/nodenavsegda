f328e28f874b983b079cb77854e3654386e006ee
Art Navsegda <artnavsegda@gmail.com>
2021-03-17 10:08:17 +0300

    bolka

--- fridge/server/index.js
+++ fridge/server/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const multer  = require('multer')
+const path = require('path')
+
 const upload = multer({ dest: 'uploads/' })
 
-app.post('/api/uploadPhoto', upload.single('avatar'), function (req, res, next) {
+const app = express()
+
+app.use(express.static(__dirname));
+
+app.post('/api/uploadPhoto', upload.single('vending_image'), function (req, res, next) {
     // req.file is the `avatar` file
     // req.body will hold the text fields, if there were any
+    res.send(`You have uploaded this image: <hr/><img src="../${req.file.path}" width="500">`);
 })
 
+app.listen(3000, () => console.log(`Listening on port 3000`));
