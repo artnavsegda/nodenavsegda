f433f4f7899cc3fb1d2192a8b6b6d6cb2bbf673c
Art Navsegda <artnavsegda@gmail.com>
2021-03-18 13:43:29 +0300

    Update index.js

--- fridge/server/index.js
+++ fridge/server/index.js
@@ -5,16 +5,27 @@ const mqtt = require('mqtt')
 
 // mqtt
 
-var client  = mqtt.connect('mqtt://84.22.105.210')
+var client  = mqtt.connect('mqtt://84.22.105.210', {
+    username: "testuser",
+    password: "testuser"
+})
 
+client.on('connect', function () {
+    client.subscribe('/client/#', function (err) {
+        //
+    })
+})
 
+client.on('message', function (topic, message) {
+    console.log("Topic: " + topic.toString() + ", Message: " +  message.toString())
+})
 
 
 
 
 // http
 
-const upload = multer({ dest: 'uploads/' })
+/* const upload = multer({ dest: 'uploads/' })
 const app = express()
 app.use(express.static(__dirname));
 
@@ -22,4 +33,4 @@ app.post('/api/uploadPhoto', upload.single('vending_image'), function (req, res,
     res.send(`You have uploaded this image: <hr/><img src="../${req.file.path}" width="500">`);
 })
 
-app.listen(3000, () => console.log(`Listening on port 3000`));
+app.listen(3000, () => console.log(`Listening on port 3000`)); */
