9e485711ceb7d4a8370d569d4d422ed764a99bb2
Art Navsegda <artnavsegda@gmail.com>
2021-03-18 13:22:16 +0300

    aaaa

--- fridge/server/index.js
+++ fridge/server/index.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const multer  = require('multer')
 const path = require('path')
+const mqtt = require('mqtt')
+
+// mqtt
+
+var client  = mqtt.connect('mqtt://84.22.105.210')
+
+
+
+
+
+
+// http
 
 const upload = multer({ dest: 'uploads/' })
-
 const app = express()
-
 app.use(express.static(__dirname));
 
 app.post('/api/uploadPhoto', upload.single('vending_image'), function (req, res, next) {
-    // req.file is the `avatar` file
-    // req.body will hold the text fields, if there were any
     res.send(`You have uploaded this image: <hr/><img src="../${req.file.path}" width="500">`);
 })
 
