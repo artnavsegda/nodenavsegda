4f474e20430535a1f226af2b6392cdb6e9a61f80
Art Navsegda <artnavsegda@gmail.com>
2021-04-14 12:36:44 +0300

    Update index.js

--- webserver/index.js
+++ webserver/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const multer  = require('multer')
 const app = express()
+const upload = multer({ dest: 'uploads/' })
 app.use(express.json())
 const port = 3000
 
@@ -11,6 +12,10 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/q', (req, res) => {
   console.log("query " + req.query.ask);
   res.send(dictionary.filter((element) => element.startsWith(req.query.ask)));
+})
+
+app.post('/upload', upload.single('image'), function (req, res) {
+  res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500">`);
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
