474677d216ac6ceb75f0359ea75aa05fa23e0a5b
Art Navsegda <artnavsegda@gmail.com>
2022-03-15 16:00:54 +0300

    Update index.js

--- useless/index.js
+++ useless/index.js
@@ -1,11 +1,12 @@
 
 const express = require('express')
 const fs = require('fs');
+const path = require('path');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  fs.readdir('./', (err, files) => {
+  fs.readdir('~/eff/', (err, files) => {
     res.send(files);
   });
 })
